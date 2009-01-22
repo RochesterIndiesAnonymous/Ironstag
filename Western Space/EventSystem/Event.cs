8e9760336bda2661c05644c78818f030d181ef59
iarejenius <iarejenius@f4ea5768-c5f2-11dd-ba5c-d3b7c2b3b534>
2009-01-22 04:30:06 +0000

    Initial commit of Event system. Also, added rule to ignore .csproj, another auto-generated file.

---
+++ Western Space/EventSystem/Event.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace WesternSpace.EventSystem
+{
+    /**
+     * An Event class is a generic container for invoking
+     * some "action" on an object. Only methods that don't
+     * have any arguments are supported right now.
+     */
+    class Event
+    {
+        private Object host;
+        private MethodInfo method;
+        private Object[] parameters;
+
+        /**
+         * Construct an Event object that executes a method without
+         * any arguments.
+         */
+        public Event(Object host, String methodName)
+        {
+            this.host = host;
+            this.method = host.GetType().GetMethod(methodName);
+
+            parameters = new Object[0];
+        }
+
+        /**
+         * Execute the action.
+         */
+        public void execute()
+        {
+            Object returnVal = method.Invoke(host, parameters);
+            Console.WriteLine(returnVal);
+        }
+    }
+}
