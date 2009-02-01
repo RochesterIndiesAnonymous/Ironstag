b214e251cd6d62fa04e4e1e9607f20115e5c20b4
WebFinesse <WebFinesse@f4ea5768-c5f2-11dd-ba5c-d3b7c2b3b534>
2009-02-01 04:03:51 +0000

    Initial framework for damaging and damageable

---
+++ Western Space/Interfaces/IDamaging.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WesternSpace.Utility;
+
+namespace WesternSpace.Interfaces
+{
+    /// <summary>
+    /// This interface is used to provide information about how much
+    /// damage is to be done to the player
+    /// 
+    /// All items in the world that do damage to the player
+    /// need to implement this class.
+    /// </summary>
+    public interface IDamaging
+    {
+        /// <summary>
+        /// The owner of of the damage.
+        /// For projectiles this would be a reference to the enemy that fired the projectile
+        /// For enemies this would just be a refence to itself
+        /// </summary>
+        object Owner
+        {
+            get;
+        }
+
+        /// <summary>
+        /// What this item or character is seeking to damage. If this is an enemy character or
+        /// enemy projectile, set to Player. If this is the Player or player projectile set to Enemy
+        /// </summary>
+        DamageCategory DoesDamageTo
+        {
+            get;
+        }
+
+        /// <summary>
+        /// The amount of damage this character/projectile does to the player
+        /// </summary>
+        int AmountOfDamage
+        {
+            get;
+        }
+    }
+}
---
+++ Western Space/Utility/DamageCategory.cs
@@ -0,0 +1,21 @@
+﻿
+namespace WesternSpace.Utility
+{
+    /// <summary>
+    /// This categorizes the the type of damage that happens in the world.
+    /// This is needed by the collision framework to check and see if a collision
+    /// matters or not.
+    /// </summary>
+    public enum DamageCategory
+    {
+        /// <summary>
+        /// The Player category. 
+        /// </summary>
+        Player,
+        
+        /// <summary>
+        /// The Enemy category.
+        /// </summary>
+        Enemy
+    }
+}
