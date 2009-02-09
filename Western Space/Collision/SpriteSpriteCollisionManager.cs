9574caa47441ca8330e5edcb7d1d8d418ffa998b
sirbrettmosley <sirbrettmosley@f4ea5768-c5f2-11dd-ba5c-d3b7c2b3b534>
2009-02-09 06:08:44 +0000

    Added Comments

--- Western Space/Collision/SpriteSpriteCollisionManager.cs
+++ Western Space/Collision/SpriteSpriteCollisionManager.cs
@@ -13,10 +13,15 @@ using WesternSpace.ServiceInterfaces;
 
 namespace WesternSpace.Collision
 {
+    // SpriteSpriteCollisionManager - is a grid based collision management system
+    // You pretty much add a ISpriteCollideable interface to an object and 
+    // register it with this manager and this takes care of the collision detection
+    // between the objects. It currently supports Bounding Box Algorithm
     public class SpriteSpriteCollisionManager : DrawableGameComponent //GameComponent
-    {
+    {        
+        static Boolean debug = true;
+        // IDNumberCounter gets incremented every time a new object is added
         protected int IDNumberCounter = 0;
-        static Boolean debug = false;
         // Object Collision Grid 
         protected CollisionObjectBin[,] objectCollisionGrid;
         // Number Of Bins
@@ -27,6 +32,8 @@ namespace WesternSpace.Collision
         protected int binHeight;        
         // Registered Object List
         protected List<ISpriteCollideable> registeredObject;
+        // List of Registered Objects to be removed
+        protected List<ISpriteCollideable> registeredObjectRemoveList;
         // Object Bins To Check (This is added to in Grid Bins when a bin contains multiple sprites)
         protected List<CollisionObjectBin> objectBinsToCheck;
         public List<CollisionObjectBin> ObjectBinsToCheck
@@ -44,12 +51,14 @@ namespace WesternSpace.Collision
         public SpriteSpriteCollisionManager(Game game, ISpriteBatchService spriteBatch, int binWidth, int binHeight)
             : base(game)
         {
-            registeredObject = new List<ISpriteCollideable>();
-            objectBinsToCheck = new List<CollisionObjectBin>();            
-            objBinLookupTable = new Dictionary<int, List<Point>>();
-            refSpriteBatch = spriteBatch.GetSpriteBatch("Camera Sensitive");
+            this.registeredObject = new List<ISpriteCollideable>();
+            this.objectBinsToCheck = new List<CollisionObjectBin>();
+            this.objBinLookupTable = new Dictionary<int, List<Point>>();
+            this.refSpriteBatch = spriteBatch.GetSpriteBatch("Camera Sensitive");
             this.binWidth = binWidth;
             this.binHeight = binHeight;
+            this.gridWidth = 0;
+            this.gridHeight = 0;
         }
         public override void Initialize()
         {
@@ -66,82 +75,103 @@ namespace WesternSpace.Collision
             }
             base.Initialize();
         }
-        public Boolean findOutIfObjectIsInCameraSpace(ISpriteCollideable collideableObject)
+        // Adds an object to the registered object list 
+        // (Adds a object to a bin on the next update because the 
+        // camera may not have been created yet)
+        public void addObjectToRegisteredObjectList(ISpriteCollideable collideableObject)
         {
-            if (findOutIfPointIsInCameraSpace(collideableObject.Rectangle.Left, collideableObject.Rectangle.Top) &&
-                findOutIfPointIsInCameraSpace(collideableObject.Rectangle.Right, collideableObject.Rectangle.Bottom))            
-                return true;            
+            // Registers a Collideable Object           
+            registeredObject.Add(collideableObject);
+            // assigns a id number to object
+            collideableObject.IdNumber = this.IDNumberCounter;           
+            // increment the idnumberCounter
+            IDNumberCounter++;            
+            //Debug.Print("Object Added to Registered List: " + collideableObject.IdNumber);
+        }
+        // Removes an object from the registered object list
+        // the object is removed immediately
+        public void removeObjectFromRegisteredObjectList(ISpriteCollideable collideableObject)
+        {
+            // Removes the object from the registered object list
+            registeredObject.Remove(collideableObject);
+            // Remove Object from the object bin lookup table 
+            objBinLookupTable.Remove(collideableObject.IdNumber);
+            
+
+            // Removes object directly from the object bin
+            OnRemoveObjectFromBin(collideableObject, this.getObjectCollisionBinCoord(collideableObject));                        
+         
+            // Debug.Print("Object Removed from Registered List: " + collideableObject.IdNumber);
+        }
+        // Finds out if the object is in camera space
+        protected Boolean findOutIfObjectIsInCameraSpace(ISpriteCollideable collideableObject)
+        {
+            Rectangle objectRectangle = collideableObject.Rectangle;
+            if ((objectRectangle.Left >= camera.VisibleArea.Left) &&
+                (objectRectangle.Right < camera.VisibleArea.Right) &&
+                (objectRectangle.Top >= camera.VisibleArea.Top) &&
+                (objectRectangle.Bottom < camera.VisibleArea.Bottom))
+                return true;
             return false;
         }
-        public Boolean findOutIfPointIsInCameraSpace(float x, float y)
+        // Finds out if point is in camera space
+        protected Boolean findOutIfPointIsInCameraSpace(float x, float y)
         {
             if (x >= camera.VisibleArea.Left && x < camera.VisibleArea.Right &&
                 y >= camera.VisibleArea.Top && y < camera.VisibleArea.Bottom)
                 return true;
             return false;
         }
-        public void addObjectToRegisteredObjectList(ISpriteCollideable collideableObject)
-        {
-            // add Object to Bins
-            registeredObject.Add(collideableObject);
-            // assigns a id number to object
-            collideableObject.IdNumber = this.IDNumberCounter;
-            //Debug.Print("Object Added to Registered List: " + collideableObject.IdNumber);
-            // increments the idnumberCounter 
-            IDNumberCounter++;
-        }
-        public void removeObjectFromRegisteredObjectList(ISpriteCollideable collideableObject)
-        {
-            // 
-            this.objBinLookupTable.Remove(collideableObject.IdNumber);
-            OnRemoveObjectFromBin(collideableObject, this.getObjectCollisionBinCoord(collideableObject));            
-            // Debug.Print("Object Removed from Registered List: " + collideableObject.IdNumber);
-            // Removes the registered Object
-            registeredObject.Remove(collideableObject);
-        }
+        // Adds object to the coresponding object bins
         protected void OnAddObjectToBin(ISpriteCollideable collideableObject, List<Point> listOfObjectBinCoord)
         {
+            // loop though the coordinates add the object to the
+            // coresponding grid space
             foreach (Point binCoord in listOfObjectBinCoord)
             {
                 // Add Object to Object Collision Grid
-                this.objectCollisionGrid[binCoord.X, binCoord.Y].OnObjectAdded(collideableObject);
+                if (binCoord.X >= 0 && binCoord.X < gridWidth && binCoord.Y >= 0 && binCoord.Y < gridHeight)
+                    this.objectCollisionGrid[binCoord.X, binCoord.Y].OnObjectAdded(collideableObject);
             }
-            //// Update Look Up Table               
+            // Update Look Up Table               
             this.objBinLookupTable[collideableObject.IdNumber] = listOfObjectBinCoord;
         }
+        // Removes object from the corresponding object bins
         protected void OnRemoveObjectFromBin(ISpriteCollideable collideableObject, List<Point> listOfObjectBinCoord)
         {
+            // loop though the coordinates remove the object to the
+            // coresponding grid space
             foreach (Point binCoord in listOfObjectBinCoord)
             {
-                // Remove Object to ObjectList of a Bin
+                // Check if the coordinate is within the grid space
+                // Remove Object from the ObjectList of a Bin
                 if (binCoord.X >= 0 && binCoord.X < gridWidth && binCoord.Y >= 0 && binCoord.Y < gridHeight)
                     this.objectCollisionGrid[binCoord.X, binCoord.Y].OnObjectRemoved(collideableObject);
             }
-            //// Update Object Look Up Table               
+            // Update Object Look Up Table               
             this.objBinLookupTable[collideableObject.IdNumber] = listOfObjectBinCoord;            
         }
-        public Point xformScreenCoordToBinCoord(Vector2 vector)
+        // Transform World Coordinates to Bin Coordinates
+        protected Point WorldCoordToBinCoord(Vector2 vector)
         {
             // CameraSpace
             float x = (vector.X - this.camera.VisibleArea.X);
             float y = (vector.Y - this.camera.VisibleArea.Y);
             // GridSpace
             return new Point((int)x / binWidth, (int)y / binHeight);                       
-        }
-        /*
-         * Changed NewRectToCoord = getObjectCollisionBinCoord
-         * getObjectCollisionBinCoord - Takes the upperLeft and the lowerRight corner points
-         * and interpolates between their x and y values to determin all of the
-         * grid points they occupy         
-         */
-        public List<Point> getObjectCollisionBinCoord(ISpriteCollideable collideableObject)
+        }     
+        // Gets the Objects World Coordinates/Rectangle and transforms them into Bin Coordinates
+        // Works by taking the topleft and the bottom right and interpolates to get all the
+        // coordinates in between.
+        protected List<Point> getObjectCollisionBinCoord(ISpriteCollideable collideableObject)
         {
             List<Point> listOfBinCoord = new List<Point>();
-            Point leftTop = xformScreenCoordToBinCoord(
+            Point leftTop = WorldCoordToBinCoord(
                 new Vector2(collideableObject.Rectangle.Left, collideableObject.Rectangle.Top));
-            Point rightBottom = xformScreenCoordToBinCoord(
+            Point rightBottom = WorldCoordToBinCoord(
                 new Vector2(collideableObject.Rectangle.Right, collideableObject.Rectangle.Bottom));
-            
+            // If the left top and right bottom are not equal
+            // scan through the points
             if (!leftTop.Equals(rightBottom))
             {
                 for (int y = leftTop.Y; y <= rightBottom.Y; y++)
@@ -152,13 +182,15 @@ namespace WesternSpace.Collision
                     }
                 }
             }
+            // if the left top and bottom right are equal add just the left top
             else
             {                
                 listOfBinCoord.Add(leftTop);
             }
             return listOfBinCoord;
         }
-        public Boolean CoordsEqual(List<Point> pointA, List<Point> pointB)
+        // Checks if 2 Lists of Coordinates are equal
+        protected Boolean CoordsEqual(List<Point> pointA, List<Point> pointB)
         {
             if (pointA.Count != pointB.Count)
                 return false;
@@ -170,23 +202,28 @@ namespace WesternSpace.Collision
             return true;
         }
         public override void Update(GameTime gameTime)
-        {
+        {         
             List<Point> newCoords;
             List<Point> oldCoords;
             // Update Collision Bins
+            // This is the list of register game objects
             foreach (ISpriteCollideable gameObj in registeredObject)
-            {                
+            {
+                // We get the new position of the game object
                 newCoords = this.getObjectCollisionBinCoord(gameObj);
                 // Lookup table contains the last coordnates of each game object
                 if (objBinLookupTable.TryGetValue(gameObj.IdNumber, out oldCoords))
-                {                     
+                {
+                    // We find out if the object is in camera space
+                    // if it is not we remove it from the object bin 
                     if (findOutIfObjectIsInCameraSpace(gameObj) == false)
                     {
                         OnRemoveObjectFromBin(gameObj, oldCoords);
                     }
+                    // if it is not we add it to the object bins
                     else
                     {
-                        // Update Bin Only if the coords have changed
+                        // We only update the bins that the coordnates have changed
                         if (!CoordsEqual(oldCoords, newCoords))
                         {
                             //Debug.Print(gameObj.IdNumber + " Occupies ");
@@ -194,13 +231,21 @@ namespace WesternSpace.Collision
                             //{
                             //    Debug.Print(">" + coord.ToString());
                             //}
+                            // Remove object from the bin
                             OnRemoveObjectFromBin(gameObj, oldCoords);
-                            OnAddObjectToBin(gameObj, newCoords);                            
+                            // Add object to the bin
+                            OnAddObjectToBin(gameObj, newCoords);
                         }
-                    }                    
+                    }
                 }
+                // if the object is not in the lookup table we add it to 
+                // the lookup table (this is for newly registered objects)
+                // we do this here instead of the registeredfunction because this 
+                // function is run after the camera is created
                 else
                 {
+                    // first we find out if the object is in the
+                    // cameraspace
                     if (findOutIfObjectIsInCameraSpace(gameObj))
                     {
                         //Debug.Print("Camera Pos:\n>" + camera.VisibleArea.ToString() +
@@ -212,26 +257,34 @@ namespace WesternSpace.Collision
                         this.OnAddObjectToBin(gameObj, newCoords);
                     }
                 }
-            }
+            }            
             // Make a copy of the bins to check so when a bin is removed the loop doesnt freak out 
             IEnumerable<CollisionObjectBin> objBinsToCheckCopy = objectBinsToCheck.ToList();
             // Scan all bins on the Object List to be Checked
             foreach (CollisionObjectBin gameObjBin in objBinsToCheckCopy)
-            {                
+            {
+                // We take two indexers Start i at 0 and end at n-1
+                // and the second we start at j at 1 and at n
+                // We do this to ensure that the none of the objects are checked against 
+                // it self reducing the number of caluclations
+                // A, B, C, D
+                // This way (A,B),(A,C),(A,D),(B,C),(B,D),(C,D) = 6 calculations vs 16 
+                // with a index that starts at zero
                 for (int i = 0; i < gameObjBin.ListOfCollideableObjects.Count - 1; i++)
                 {
                     for (int j = 1; j < gameObjBin.ListOfCollideableObjects.Count; j++)
                     {
-                        if (BoundingBox(gameObjBin.ListOfCollideableObjects.ElementAt(i), gameObjBin.ListOfCollideableObjects.ElementAt(j)))
-                        //if (PixelCollision(gameObjBin.ListOfCollideableObjects.ElementAt(i), gameObjBin.ListOfCollideableObjects.ElementAt(j), gameTime))
+                        if (BoundingBox(gameObjBin.ListOfCollideableObjects.ElementAt(i), gameObjBin.ListOfCollideableObjects.ElementAt(j)))                        
                         {
+                            // Copys the collideable object if one of them gets deleted before the other
                             ISpriteCollideable collidedObj1 = gameObjBin.ListOfCollideableObjects.ElementAt(i);
                             ISpriteCollideable collidedObj2 = gameObjBin.ListOfCollideableObjects.ElementAt(j);
                             collidedObj1.OnSpriteCollision(collidedObj2);
-                            collidedObj2.OnSpriteCollision(collidedObj1);                            
+                            collidedObj2.OnSpriteCollision(collidedObj1);
                         }
                     }
-                }             
+                }
+            
             }
             base.Update(gameTime);
         }
